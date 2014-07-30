61f9d908f1a8df1fcf37fed1f19e6e69a0f8a358
Saamahn Mahjouri <sm5401@nyu.edu>
2014-07-30 16:04:42 +0000

    Added Paperclip

---
+++ .c9/metadata/preview-https:/dogsitter-c9-mahjouri.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://dogsitter-c9-mahjouri.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://dogsitter-c9-mahjouri.c9.io/users/sign_in","previewer":"preview.browser"},"timestamp":1406731017139,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140729215527) do
+ActiveRecord::Schema.define(version: 20140730151055) do
 
   create_table "dogs", force: true do |t|
     t.string   "breed"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 20140729215527) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "sitters", force: true do |t|
@@ -37,6 +41,10 @@ ActiveRecord::Schema.define(version: 20140729215527) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "sitters", ["dog_id"], name: "index_sitters_on_dog_id"
