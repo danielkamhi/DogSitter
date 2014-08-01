094fbf82cb4669b120a772ec9a33e4fd9775e230
Daniel Kamhi <danielkamhi@gmail.com>
2014-08-01 10:54:54 -0400

    day 5 search

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   default from: "pae.oconnor@gmail.com"
 
-  def password_changed(id)
+  def password_changed id
     @user = User.find(id)
     mail to: @user.email, subject: "Your password has changed"
   end
